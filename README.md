3d09a9f0af0ad39faea768e480fd4539cd424a6f
jahedev <jahedev@gmail.com>
2022-03-26 00:05:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Random Learning
+
+- Here, I keep track of different technologies, libraries, ideas, and other stuff I am learning or testing, which are too miniature to deserve their own repo.
+- This makes it easier to look at a glance of everything I'm learning in one place.
---
+++ tutorials/html_canvas/script.js
@@ -0,0 +1,75 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+/*
+
+// fillRect()
+ctx.fillStyle = 'red'
+ctx.fillRect(20, 20, 150, 100)
+ctx.fillStyle = 'blue'
+ctx.fillRect(40, 40, 150, 100)
+
+// strokeRect()
+ctx.lineWidth = 5
+ctx.strokeStyle = 'green'
+ctx.strokeRect(100, 200, 150, 100)
+
+// clearRect()
+ctx.clearRect(30, 30, 150, 100)
+
+// fillText()
+ctx.font = '30px Arial'
+ctx.fillStyle = 'purple'
+ctx.fillText('Hello World', 400, 50)
+
+// strokeText()
+ctx.lineWidth = 1
+ctx.strokeStyle = 'orange'
+ctx.strokeText('Hello Again', 400, 100)
+
+*/
+/*  
+// Paths
+ctx.beginPath()
+ctx.moveTo(50, 50)
+ctx.lineTo(150, 50)
+ctx.lineTo(100, 150)
+// ctx.lineTo(50, 50)
+ctx.closePath()
+// ctx.stroke()
+ctx.fillStyle = 'coral'
+ctx.fill()
+
+ctx.beginPath()
+ctx.moveTo(200, 50)
+ctx.lineTo(150, 200)
+ctx.lineTo(250, 200)
+ctx.closePath()
+ctx.fillStyle = 'orange'
+ctx.fill()
+
+ctx.beginPath()
+ctx.rect(300, 50, 150, 100)
+ctx.fillStyle = 'teal'
+ctx.fill()
+*/
+
+/*
+// Arcs
+
+ctx.beginPath()
+ctx.arc(canvas.width / 2, canvas.height / 2, 250, 0, Math.PI * 2)
+ctx.fillStyle = 'tomato'
+ctx.fill()
+ctx.beginPath()
+ctx.arc(canvas.width / 3, canvas.width / 3, 40, 0, Math.PI * 2)
+ctx.fillStyle = 'aliceblue'
+ctx.fill()
+ctx.beginPath()
+ctx.arc(canvas.width / 1.6, canvas.width / 3, 40, 0, Math.PI * 2)
+ctx.fill()
+ctx.beginPath()
+ctx.arc(canvas.width / 2, canvas.height / 1.8, 100, 0, Math.PI)
+ctx.fill()
+*/
+
+// https://youtu.be/gm1QtePAYTM?t=1811
