eaeb8131b70383115c5d134822b1e574cfafdbda
Jahed Hossain <jahedev@gmail.com>
2022-11-10 18:40:01 -0500

    html canvas

--- html_canvas/script.js
+++ html_canvas/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
 
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
@@ -16,23 +17,55 @@ const mouse = {
 canvas.addEventListener('click', (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
-  drawCircle();
 });
 
 canvas.addEventListener('mousemove', (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
-  drawCircle();
 });
 
-function drawCircle() {
-  ctx.fillStyle = 'blue';
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 10, 0, Math.PI * 2);
-  ctx.fill();
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    // this.x = mouse.x;
+    // this.y = mouse.y;
+    this.size = Math.random() * 5 + 1; // 1 to 6
+    this.speedX = Math.random() * 3 - 1.5; // -1.5 to 1.5
+    this.speedY = Math.random() * 3 - 1.5; // -1.5 to 1.5
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+init();
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
 }
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
   requestAnimationFrame(animate);
 }
+
+animate();
