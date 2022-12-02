4e7427bee1dbb7b0054514c8b56d9e368aa3f30c
M. Jahed Hossain <jahedev@gmail.com>
2022-12-02 15:59:21 -0500

    regexone.com answers

--- regexp-learn/chapter4.js
+++ regexp-learn/chapter4.js
@@ -5,5 +5,12 @@ let res
 /*
  * Notes
  *
- *
+ * + any character after, any amount
+ * s* any number of s characters, but at least 1
+ * /apple?/ apple, apples, but not apples
+ * / \d\d\d\d* / 3 or more digits
+ * / \d\d\d+ / 3 or more digits
+ * /colou?r/ color or colour
+ * /[a-z]{1,3}/ 1 to 3 lowercase characters
+ * /[a-z]{3,}/ 3 or more characters
  */
