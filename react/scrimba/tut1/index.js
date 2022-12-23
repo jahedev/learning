67cdd7148a3c4b9976cb6215fb5228d2cffeb3d2
MJ Hossain <jahedev@gmail.com>
2022-12-23 15:33:02 -0500

    react

---
+++ react/scrimba/tut1/index.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div>
+            <ul>
+                <li>Hello</li>
+                <li>Wordld</li>
+                <li>!?!</li>
+            </ul>
+        </div>
+    )
+}
+
+// ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
