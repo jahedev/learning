cd452092d1d1c32ca6a0d9afa816606390c40f20
MJ. Hossain <jahedev@gmail.com>
2023-02-13 16:29:12 -0500

    ts

--- typescript/ts1.js
+++ typescript/ts1.js
@@ -1,12 +1,25 @@
-var exObj;
 var myId = 'abc123';
 var myPrice = 3.142;
 var myTags = ['hello', 'world'];
-var myObj = { title: 'SOmething', desc: 'another' };
+var myObj = {
+    title: 'SOmething',
+    desc: 'another'
+};
+var myTuple = [1, 2, '3'];
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var myEnum = Role.AUTHOR;
+var exObj;
 exObj = {
     id: myId,
     price: myPrice,
     tags: myTags,
-    obj: myObj
+    obj: myObj,
+    tupl: myTuple,
+    enm: myEnum
 };
-console.log('hello world');
+console.log(exObj);
--- typescript/ts1.ts
+++ typescript/ts1.ts
@@ -1,4 +1,3 @@
-let exObj: object
 const myId: string = 'abc123'
 const myPrice: number = 3.142
 const myTags: string[] = ['hello', 'world']
@@ -6,12 +5,27 @@ const myObj: { title: string; desc: string } = {
   title: 'SOmething',
   desc: 'another',
 }
+const myTuple: [number, number, string] = [1, 2, '3']
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+const myEnum: Role = Role.AUTHOR
+const myFunc: (n: number, m: number) => number = function (a, b) {
+  return a * b
+}
+
+let exObj: object
 
 exObj = {
   id: myId,
   price: myPrice,
   tags: myTags,
   obj: myObj,
+  tupl: myTuple,
+  enm: myEnum,
+  fun: myFunc,
 }
 
-console.log('hello world')
+console.log(exObj)
