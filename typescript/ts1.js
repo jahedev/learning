80b5c9c6cf8adea5c7dc65317809467dd35cc56b
MJ. Hossain <jahedev@gmail.com>
2023-02-15 15:37:09 -0500

    ts practice

---
+++ typescript/ts1.js
@@ -0,0 +1,12 @@
+var exObj;
+var myId = 'abc123';
+var myPrice = 3.142;
+var myTags = ['hello', 'world'];
+var myObj = { title: 'SOmething', desc: 'another' };
+exObj = {
+    id: myId,
+    price: myPrice,
+    tags: myTags,
+    obj: myObj
+};
+console.log('hello world');
---
+++ typescript/ts1.ts
@@ -0,0 +1,17 @@
+let exObj: object
+const myId: string = 'abc123'
+const myPrice: number = 3.142
+const myTags: string[] = ['hello', 'world']
+const myObj: { title: string; desc: string } = {
+  title: 'SOmething',
+  desc: 'another',
+}
+
+exObj = {
+  id: myId,
+  price: myPrice,
+  tags: myTags,
+  obj: myObj,
+}
+
+console.log('hello world')
