dc4297db62a32c79629d06864760e28d27181a05
MJ. Hossain <jahedev@gmail.com>
2023-03-02 17:45:51 -0500

    table aliases

--- sql/Pets Joining.sql
+++ sql/Pets Joining.sql
@@ -11,3 +11,10 @@ FROM [Pets]
 LEFT JOIN [Owners]
 	ON [Pets].OwnerID = [Owners].OwnerID
 ORDER BY [Owners].OwnerID
+
+------ ALIASES FOR TABLES ------
+SELECT P.[Name], O.[Name]
+FROM [Pets] AS P
+LEFT JOIN [Owners] AS O
+ON P.[OwnerID] = O.[OwnerID]
+WHERE LEFT(P.[Name], 1) = LEFT(O.[Name], 1)
